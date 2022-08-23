e89b9e07a5dc234b225677ed261366d29556aa13
Nikolaos Bellias <nmpellias@cognity.gr>
2022-08-23 14:52:46 +0300

    Add project files.

---
+++ MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClasses
+{
+    public class MyClass
+    {
+        public static void DoSomething(DateTime dt)
+        {
+            Console.WriteLine(dt.ToUniversalTime());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StaticClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Static classes");
+            MyClass.DoSomething(DateTime.Now);
+            Thread.Sleep(2000);
+            MyClass.DoSomething(DateTime.Now);
+            Thread.Sleep(2000);
+            MyClass.DoSomething(DateTime.Now);
+        }
+    }
+}
